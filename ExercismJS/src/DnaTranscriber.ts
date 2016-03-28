da2e90fefe25042f027a17625e8ccfe146d65224
Eric Lobdell <eric@ericlobdell.me>
2016-03-28 16:54:24 -0400

    finished  rna-transcription

---
+++ ExercismJS/src/DnaTranscriber.ts
@@ -0,0 +1,13 @@
+﻿
+class DnaTranscriber {
+
+    toRna( dna: string ): string {
+        const rnaMap = { C: "G", G: "C", A: "U", T: "A" };
+
+        return dna
+            .split( '' )
+            .map( c => rnaMap[c] )
+            .join( '' );
+
+    }
+}
