a7880411bc4043df47a226ab9a7525a4524778a5
Eric Lobdell <eric@ericlobdell.me>
2016-03-28 16:09:12 -0400

    initial commit. Finished leap

---
+++ ExercismJS/src/leap.ts
@@ -0,0 +1,26 @@
+﻿
+class Year {
+
+    year: number;
+
+    constructor( year: number ){
+        this.year = year;
+    }
+
+    isLeap( ) {
+        if ( isDivisibleBy( this.year, 4 ) ) {
+
+            if ( isDivisibleBy( this.year, 100 ) )
+                return isDivisibleBy( this.year, 400 );
+            else
+                return true;
+        }
+        else
+            return false;
+    }
+
+}
+
+function isDivisibleBy( num: number, test: number ) {
+    return num % test === 0;
+}
---
+++ ExercismJS/wallaby.config.js
@@ -0,0 +1,12 @@
+﻿module.exports = function ( w ) {
+
+    return {
+        files: [
+          'src/**/*.ts'
+        ],
+
+        tests: [
+          'test/**/*.spec.js'
+        ]
+    };
+};
